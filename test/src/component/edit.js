c4ba0d0fc6ef5de2cfd46e0c48e74e4b44d9f6f7
nahid rezaeee <nahid.rezayi3@gmail.com>
2022-06-18 20:51:20 +0430

    edit

--- test/src/component/edit.js
+++ test/src/component/edit.js
@@ -1,13 +1,14 @@
-import React, { useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { editData } from "../store/actions";
 
 const Edite = (props) => {
-  const [edit, setEdit] = useState();
-
+  const state = useSelector((state) => state);
+  const id = state.editedID;
+  const [edit, setEdit] = useState(state.items[id-1]?.fullname);
+  const inputElement = useRef();
   const dipacth = useDispatch();
-  const id = useSelector((state) => state.editedID);
   const Navigate = useNavigate();
 
   const btnclickHandler = () => {
@@ -15,12 +16,18 @@ const Edite = (props) => {
     Navigate("/");
   };
 
+  useEffect(() => {
+    inputElement.current.focus();
+  }, []);
+
   return (
     <>
       <div className="editform">
         <input
+          ref={inputElement}
           type="text"
           className="input-edit"
+          value={edit}
           onChange={(e) => setEdit(e.target.value)}
         />
         <button className="btn-edit" onClick={btnclickHandler}>
